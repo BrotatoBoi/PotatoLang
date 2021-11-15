b0f9021de90cac60396971ff52d9e9a516d75032
Bruhtato <flamingburrito15@gmail.com>
2021-11-15 14:15:08 -0500

    Minor Code Updates.

--- Interpre-tato.py
+++ Interpre-tato.py
@@ -6,56 +6,68 @@
 # ~~ ------------------------------------------------ ~~ #
 
 
+import os
+
+
 # ~ Run Potato. ~ #
 def run(file):
-    variables = {}
+    print(f"Starting up file {file}")
+
+    os.system('clear' if os.name == 'posix' else 'cls')
 
     # ~ Open the file. ~ #
     with open(file, 'r') as f:
+        variables = {}
+
         # ~ Read each line ~ #
         for line in f:
             # ~ Check if the line is a comment. ~ #
             if line.startswith('$'):
                 # ~ If it is, skip it. ~ #
                 continue
-            # ~ If it isn't, split it into a list. ~ #
-            line = line.split()
+            else:
+                # ~ If it isn't, split it into a list. ~ #
+                line = line.split()
 
-            # ~ Check if the line is not empty. ~ #
-            if line:
-                if line[0] == 'say':
-                    inString = False
-                    output = ''
-                    message = ' '.join(line[1:]).split(' ')
+                # ~ Check if the line is not empty. ~ #
+                if line:
+                    if line[0] == 'say':
+                        inString = False
+                        output = ''
+                        message = ' '.join(line[1:]).split(' ')
 
-                    # ~ Loop through the message. ~ #
-                    for word in message:
-                        # ~ Check if the word is a variable. ~ #
-                        if word in variables:
-                            # ~ If it is, add it to the output. ~ #
-                            output += variables[word]
-                        # ~ If it isn't, add it to the output. ~ #
-                        else:
-                            for char in word:
-                                if char == '"':
-                                    inString = not inString
-                                elif inString:
-                                    output += char
+                        # ~ Loop through the message. ~ #
+                        for word in message:
+                            # ~ Check if the word is a variable. ~ #
+                            if word in variables:
+                                # ~ If it is, add it to the output. ~ #
+                                output += variables[word]
+                            # ~ If it isn't, add it to the output. ~ #
+                            else:
+                                for char in word:
+                                    if char == '"':
+                                        inString = not inString
+                                    elif inString:
+                                        output += char
 
-                            output += ' '
+                                output += ' '
 
-                    # ~ Print the output. ~ #
-                    print(output)
+                        # ~ Print the output. ~ #
+                        print(output)
 
-                # ~ Check if the line is a variable. ~ #
-                elif line[0] == 'set':
-                    # ~ If it is, set the variable. ~ #
-                    variables[line[1]] = ' '.join(line[2:])
+                    # ~ Check if the line is a variable. ~ #
+                    elif line[0] == 'set':
+                        # ~ If it is, set the variable. ~ #
+                        variables[line[1]] = ' '.join(line[2:])
 
-                # ~ Check if the line is user input. ~ #
-                elif line[0] == 'get':
-                    # ~ If it is, get the user input. ~ #
-                    variables[line[1]] = input(' '.join(line[2:]))
+                    # ~ Check if the line is user input. ~ #
+                    elif line[0] == 'get':
+                        # ~ If it is, get the user input. ~ #
+                        variables[line[1]] = input(' '.join(line[2:])+' ')
+
+                    else:
+                        print(f"ERROR ON: {line}")
+                        break
 
 # ~ Main Loop. ~ #
 while True:
